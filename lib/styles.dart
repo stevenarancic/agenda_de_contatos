edbac957f49900ff7584b1099df152f329f3b17f
Steve Narancic <58119793+stevenarancic@users.noreply.github.com>
2021-08-10 15:18:27 -0300

    Update styles.dart
    criação do bluetheme, cor com paleta do 50 ao 900.

--- lib/styles.dart
+++ lib/styles.dart
@@ -3,3 +3,16 @@ import 'package:flutter/material.dart';
 Color redTheme = Color(0xffe83151);
 Color grayTheme = Color(0xff494d53);
 Color grayDivider = Color(0xffe4e4e4);
+
+MaterialColor blueTheme = MaterialColor(0xFF3771E7, {
+  50: Color(0xFFF5F8FE),
+  100: Color(0xFFEBF1FD),
+  200: Color(0xFFCDDCF9),
+  300: Color(0xFFADC5F6),
+  400: Color(0xFF739CEF),
+  500: Color(0xFF3771E7),
+  600: Color(0xFF3165CE),
+  700: Color(0xFF21448B),
+  800: Color(0xFF193368),
+  900: Color(0xFF102143),
+});
